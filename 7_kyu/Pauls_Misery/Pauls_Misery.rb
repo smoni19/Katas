4f6f0fdbc7a052d881ef7f9c41687a4e9403a45c
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-02-08 16:47:34 +0100

    Activity values are summed up

--- 7_kyu/Pauls_Misery/Pauls_Misery.rb
+++ 7_kyu/Pauls_Misery/Pauls_Misery.rb
@@ -1,3 +1,13 @@
 def paul(x)
-  return 'Super happy!'
+  paulMiseryScore = 0
+  values = {
+    "kata" => 5,
+    "Petes kata" => 10,
+    "life" => 0,
+    "eating" => 1
+  }
+  x.each do |activity|
+    paulMiseryScore += values[activity].to_i
+  end
+  p paulMiseryScore
 end
--- 7_kyu/Pauls_Misery/spec/paul_spec.rb
+++ 7_kyu/Pauls_Misery/spec/paul_spec.rb
@@ -1,7 +1,10 @@
 require './Pauls_Misery.rb'
 
 describe "Basic Tests" do
-  it "Simple Cases" do
+  it "Returns 'Super happy!' when the Misery Score is below 40" do
     expect(paul(['eating'])).to eq('Super happy!')
+  end
+  it "Returns 'Happy!' when the Misery Score is below 70 and greater than or equal to 40" do
+    expect(paul(['Petes kata', 'Petes kata', 'Petes kata', 'Petes kata', 'Petes kata'])).to eq('Happy!')
   end
 end
