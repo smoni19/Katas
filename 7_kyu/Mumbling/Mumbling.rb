9a700ffc8e97f360782ef8cef1bd8d9faf90ccf9
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-02-09 11:58:15 +0100

    Supplied string split and character amount increased correctly

--- 7_kyu/Mumbling/Mumbling.rb
+++ 7_kyu/Mumbling/Mumbling.rb
@@ -1,3 +1,9 @@
-def accum(letters)
-  return "A-Bb-Ccc-Dddd"
+def accum(string)
+  output = ""
+  string.each_char.with_index { |char, index|
+    index += 1
+    index.times { output += char }
+    output += '-'
+  }
+  p output
 end
--- 7_kyu/Mumbling/spec/mumbling_spec.rb
+++ 7_kyu/Mumbling/spec/mumbling_spec.rb
@@ -4,4 +4,7 @@ describe "Basic Tests" do
   it "returns A-Bb-Ccc-Dddd when given abcd" do
     expect(accum("abcd")).to eq("A-Bb-Ccc-Dddd")
   end
+  it "returns R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy when given RqaEzty" do
+    expect(accum("RqaEzty")).to eq("R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy")
+  end
 end
