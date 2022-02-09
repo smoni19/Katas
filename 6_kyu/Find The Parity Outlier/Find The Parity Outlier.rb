9595f65e2ebca0437d5467dcd4fd938f1996e49d
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-02-09 14:55:17 +0100

    All tests passing

--- 6_kyu/Find The Parity Outlier/Find The Parity Outlier.rb
+++ 6_kyu/Find The Parity Outlier/Find The Parity Outlier.rb
@@ -1,3 +1,8 @@
 def find_outlier(integers)
-  return 11
+  evens = []
+  odds = []
+  integers.each { | integer |
+    integer.even? ? evens.push(integer) : odds.push(integer)
+  }
+  evens.size > odds.size ? odds[0] : evens[0]
 end
--- 6_kyu/Find The Parity Outlier/spec/Find The Parity Outlier_spec.rb
+++ 6_kyu/Find The Parity Outlier/spec/Find The Parity Outlier_spec.rb
@@ -1,7 +1,10 @@
 require './Find The Parity Outlier.rb'
 
 describe "Basic Tests" do
-  it "returns A-Bb-Ccc-Dddd when given abcd" do
+  it "returns 11 when given [2, 4, 0, 100, 4, 11, 2602, 36]" do
     expect(find_outlier([2, 4, 0, 100, 4, 11, 2602, 36])).to eq(11)
+  end
+  it "returns 160 when given [160, 3, 1719, 19, 11, 13, -21]" do
+    expect(find_outlier([160, 3, 1719, 19, 11, 13, -21])).to eq(160)
   end
 end
