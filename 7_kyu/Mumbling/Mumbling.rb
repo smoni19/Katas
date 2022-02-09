529717502bb88a8402da3aeb2e5d700ab8e37124
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-02-09 14:32:30 +0100

    All tests passing

--- 7_kyu/Mumbling/Mumbling.rb
+++ 7_kyu/Mumbling/Mumbling.rb
@@ -1,9 +1,10 @@
 def accum(string)
-  output = ""
+  output = "-"
   string.each_char.with_index { |char, index|
     index += 1
-    index.times { output += char }
+    index.times { output += char.downcase }
     output += '-'
   }
-  p output
+  output.gsub!(/-([a-z])/){'-'+$1.upcase}.chop!.slice!(0)
+  return output
 end
--- 7_kyu/Mumbling/spec/mumbling_spec.rb
+++ 7_kyu/Mumbling/spec/mumbling_spec.rb
@@ -7,4 +7,7 @@ describe "Basic Tests" do
   it "returns R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy when given RqaEzty" do
     expect(accum("RqaEzty")).to eq("R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy")
   end
+  it "returns C-Ww-Aaa-Tttt" do
+    expect(accum("cwAt")).to eq("C-Ww-Aaa-Tttt")
+  end
 end
