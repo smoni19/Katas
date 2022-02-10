bdcd1e1517e3d0d87d25025b37e4a1af9ec2c061
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-02-10 13:49:01 +0100

    strng argument converted into array of numbers and digits of those numbers added together and inserted into weight array

--- 5 kyu/Ruby/Weight for weight/Weight for weight.rb
+++ 5 kyu/Ruby/Weight for weight/Weight for weight.rb
@@ -1,3 +1,20 @@
+=begin
+  ------------------------ Approach ------------------------ 
+  1. convert string to array of numbers
+  2. add digits of each number together, insert this number into weight array
+  3. order supplied string based upon weight array
+  4. return ordered string
+=end
+
+
 def order_weight(strng)
-  return "2000 103 123 4444 99"
+  numbers = strng.split(' ')
+  weights = []
+  numbers.map do | number |
+    weight = 0
+    number.to_s.chars.map do | digit |
+      weight += digit.to_i
+    end
+    weights << weight
+  end
 end
